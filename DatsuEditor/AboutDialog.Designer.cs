59e9568940dca44fa66dfc0d70a210c73e8374ba
Aura42069 <jankonstanta@gmail.com>
2024-04-10 16:00:17 +0800

    Add license to About

--- DatsuEditor/AboutDialog.Designer.cs
+++ DatsuEditor/AboutDialog.Designer.cs
@@ -32,6 +32,13 @@
             label1 = new Label();
             button1 = new Button();
             richTextBox1 = new RichTextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            richTextBox2 = new RichTextBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
             SuspendLayout();
             // 
             // label1
@@ -57,28 +64,75 @@
             // 
             // richTextBox1
             // 
-            richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.BackColor = SystemColors.ControlLightLight;
-            richTextBox1.BorderStyle = BorderStyle.FixedSingle;
-            richTextBox1.Location = new Point(12, 32);
+            richTextBox1.BorderStyle = BorderStyle.None;
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(3, 3);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ReadOnly = true;
-            richTextBox1.Size = new Size(377, 201);
+            richTextBox1.Size = new Size(363, 167);
             richTextBox1.TabIndex = 2;
             richTextBox1.Text = "Aura - ZanLibrary + DatsuEditor (format parsing)\nKwasior - Format Structs (kwastools/kwaslib https://github.com/ThisKwasior/KwasTools)\nSomeGuyWhoPlaysMGR - LY2 Icon Idea\nbonefulDelight - MOT Icon Idea";
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 32);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(377, 201);
+            tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(richTextBox1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(369, 173);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "Credits";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(richTextBox2);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(369, 173);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "License";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.BackColor = SystemColors.ControlLightLight;
+            richTextBox2.BorderStyle = BorderStyle.None;
+            richTextBox2.Dock = DockStyle.Fill;
+            richTextBox2.Location = new Point(3, 3);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.ReadOnly = true;
+            richTextBox2.Size = new Size(363, 167);
+            richTextBox2.TabIndex = 0;
+            richTextBox2.Text = resources.GetString("richTextBox2.Text");
             // 
             // AboutDialog
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(401, 273);
-            Controls.Add(richTextBox1);
+            Controls.Add(tabControl1);
             Controls.Add(button1);
             Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "AboutDialog";
             Text = "About";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -87,5 +141,9 @@
         private Label label1;
         private Button button1;
         private RichTextBox richTextBox1;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private RichTextBox richTextBox2;
     }
 }
